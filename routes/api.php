a5eb704c269be51bd17fc2a77e75c0996478a0cc
kennycallado <kennycallado@hotmail.com>
2025-02-21 18:37:43 +0100

    ADD(routes)
    api: product group:
    - index
    - show
    - store
    - delete
    - update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('categories')->group(function () {
@@ -28,4 +29,36 @@ Route::prefix('categories')->group(function () {
         '/{id}',
         [CategoryController::class, 'update']
     )->name('api.categories.update');
+});
+
+Route::prefix('products')->group(function () {
+    Route::get(
+        '/',
+        [ProductController::class, 'index']
+    )->name('api.products.index');
+
+    Route::get(
+        '/{id}',
+        [ProductController::class, 'show']
+    )->name('api.products.show');
+
+    Route::post(
+        '/',
+        [ProductController::class, 'store']
+    )->name('api.products.store');
+
+    Route::delete(
+        '/{id}',
+        [ProductController::class, 'delete']
+    )->name('api.products.delete');
+
+    Route::put(
+        '/{id}',
+        [ProductController::class, 'update']
+    )->name('api.products.update');
+
+    Route::put(
+        '/toggle/{id}',
+        [ProductController::class, 'toggleActive']
+    )->name('api.products.toggle');
 });
